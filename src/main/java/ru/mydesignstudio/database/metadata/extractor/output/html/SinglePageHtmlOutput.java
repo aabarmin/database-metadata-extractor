0b4aa87a6a0cf37bd0de9df1cc23987b45b01898
Olesya Barmina <thelesenok@gmail.com>
2020-05-23 20:53:41 +0300

    single-file-generation

---
+++ src/main/java/ru/mydesignstudio/database/metadata/extractor/output/html/SinglePageHtmlOutput.java
@@ -0,0 +1,36 @@
+package ru.mydesignstudio.database.metadata.extractor.output.html;
+
+import lombok.SneakyThrows;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
+import ru.mydesignstudio.database.metadata.extractor.extractors.model.DatabaseMetadata;
+import ru.mydesignstudio.database.metadata.extractor.extractors.model.TableMetadata;
+
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardOpenOption;
+import java.util.List;
+
+@Component
+public class SinglePageHtmlOutput {
+    @Autowired
+    private TemplateEngine templateEngine;
+
+    @SneakyThrows
+    public void output(DatabaseMetadata databaseMetadata, TableMetadata tableMetadata, Path outputFolder) {
+        final Path outputFile = outputFolder.resolve(databaseMetadata.getSchemaName() + "." + tableMetadata.getTableName() + ".html");
+        Files.deleteIfExists(outputFile);
+        Files.createFile(outputFile);
+
+        final Context context = new Context();
+        context.setVariable("databaseMetadata", databaseMetadata);
+        context.setVariable("tableMetadata", tableMetadata);
+
+        final String content = templateEngine.process("database-table-template", context);
+        Files.write(outputFile, content.getBytes(Charset.forName("UTF-8")), StandardOpenOption.WRITE);
+    }
+
+}
