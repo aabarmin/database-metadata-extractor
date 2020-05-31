dca0d2ac166395fe8228d77aa337ca27309d4f9b
Aleksandr Barmin <barmin.alexander@gmail.com>
2020-05-31 11:08:16 +0300

    Adding resource limits

--- run_confluence.sh
+++ run_confluence.sh
@@ -4,4 +4,6 @@ docker run -d \
    -v confluence_data:/var/atlassian/application-data/confluence \
    -p 8090:8090 \
    -p 8091:8091 \
+   --cpus=1 \
+   --memory=2G \
    atlassian/confluence-server
