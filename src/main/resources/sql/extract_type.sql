e22e11d4ffd426984d37aada4ceef8e9d36b0353
Olesya Barmina <thelesenok@gmail.com>
2020-06-14 18:48:38 +0300

    change templates, added uk extract

--- src/main/resources/sql/extract_type.sql
+++ src/main/resources/sql/extract_type.sql
@@ -1,6 +1,11 @@
-select owner,
-       object_name,
-       object_type
-from   all_objects
-where  object_type in ('TABLE', 'VIEW')
-and    object_name = ? and owner = ?
+select obj.owner,
+       obj.object_name,
+       obj.object_type,
+       comm.comments
+from   all_objects obj
+left outer join all_tab_comments comm
+           on obj.object_name = comm.table_name
+          and obj.owner = comm.owner
+where  obj.object_type in ('TABLE', 'VIEW')
+and    obj.object_name = ?
+and    obj.owner = ?
